6636cb20e396e488da494febedd87818543ab0b5
Valentina Rua <valentina.rua123@gmail.com>
2019-01-10 22:44:21 -0500

    redes

--- behavior.js
+++ behavior.js
@@ -1,3 +1,14 @@
 document.addEventListener ("DOMContentLoaded", function (event) { 
+    var thumbnailElement = document.getElementById ("smart_thumbnail");
+    thumbnailElement.addEventListener ("click", function () {
+        if (thumbnailElement.className == "") {
+            thumbnailElement.className = "small";
+            console.log("grande a pequeña");
+        }
+        else{
+            thumbnailElement.className = "";
+            console.log("pequeña a grande");
+        }
+      });
 
 });
